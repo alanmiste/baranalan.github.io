66df9dd5bcbfd65b18863fe92bfe9dfd9e5cea1f
unknown <baranalaan@gmail.com>
2020-12-13 17:26:05 +0100

    check if the current time is prime

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-console.log(Date.now())
-console.log("goodbye world!")
+//console.log(Date.now())
+//console.log("goodbye world!")
 var isPrime = require('is-prime');
  
-console.log(isPrime(Date.now()));
+//console.log(isPrime(Date.now()));
 //=> true
- 
-console.log("Hello world!")
+var time1=Date.now();
+console.log("Hello")
+console.log("The current time is:"+ time1)
+console.log("Is the current time a prime number?")
 
-console.log("new change")
-
-console.log("new line")
+if(isPrime(Date.now())==true)
+    console.log("yes")
+    else   
+        console.log("No")
