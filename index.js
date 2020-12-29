75740c67b5c03198d8662543fc8a6c25519a8ee8
alanmiste <baranalaan@gmail.com>
2020-12-29 15:59:00 +0100

    add first JS code

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-//console.log(Date.now())
-//console.log("goodbye world!")
-var isPrime = require('is-prime');
- 
-//console.log(isPrime(Date.now()));
-//=> true
-var time1=Date.now();
-console.log("Hello")
-console.log("The current time is:"+ time1)
-console.log("Is the current time a prime number?")
-
-if(isPrime(Date.now())==true)
-    console.log("yes")
-    else   
-        console.log("No")
+console.log(Date.now())
